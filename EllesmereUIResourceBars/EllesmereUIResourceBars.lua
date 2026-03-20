c40456e22d7da021b0d6a82c8ec5b38d8e769cd9
JensBaumannDev <baumannjens@outlook.de>
2026-03-20 02:07:41 +0100

    fixed(resourcebars): text can now be bigger than the bar itself

--- EllesmereUIResourceBars/EllesmereUIResourceBars.lua
+++ EllesmereUIResourceBars/EllesmereUIResourceBars.lua
@@ -1498,7 +1498,6 @@ local function BuildBars()
     if sp.enabled ~= false and cachedSecondary then
         if not secondaryFrame then
             secondaryFrame = CreateFrame("Frame", "ERB_SecondaryFrame", mainFrame)
-            secondaryFrame:SetClipsChildren(true)
             secondaryFrame:SetFrameStrata("MEDIUM")
             secondaryFrame:SetFrameLevel(10)
         end
